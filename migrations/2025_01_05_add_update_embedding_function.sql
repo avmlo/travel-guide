367a2bbdb9dbbd59c3f7841eadf041a0bbbc1701
Alexander Huaung <alex@avmlo.com>
2025-11-01 16:40:56 -0400

    fix: update embedding function to accept text and cast to vector

--- migrations/2025_01_05_add_update_embedding_function.sql
+++ migrations/2025_01_05_add_update_embedding_function.sql
@@ -1,9 +1,10 @@
 -- Helper function to update destination embedding and search_text
 -- This is needed because Supabase client doesn't easily support vector column updates
+-- Accepts embedding as text (e.g., '[1.0,2.0,3.0,...]') and converts to vector type
 
 CREATE OR REPLACE FUNCTION update_destination_embedding(
   p_slug text,
-  p_embedding vector,
+  p_embedding text,  -- Accept as text, convert to vector
   p_search_text text
 )
 RETURNS void
@@ -12,7 +13,7 @@ AS $$
 BEGIN
   UPDATE destinations
   SET
-    embedding = p_embedding,
+    embedding = p_embedding::vector,  -- Cast text to vector type
     search_text = p_search_text
   WHERE slug = p_slug;
 END;
